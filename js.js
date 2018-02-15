dd8661b4c31f715f8ee5ec05784edb5d88c345b3
Marco Acierno <marcoaciernoemail@gmail.com>
2018-02-15 13:31:57 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    for (let i = 0; i < 4; ++i) {
+        const color = randomColor();
+        const div = document.createElement('div');
+        div.style.background = color;
+        document.body.appendChild(div);
+    }
+
+    requestAnimationFrame(() => document.body.classList.add('ready'));
+};
+
+// I was flying so no internet access
+function randomColor() {
+    const hexArray = [
+        toHex(Math.floor(Math.random() * 16)),
+        toHex(Math.floor(Math.random() * 16)),
+        toHex(Math.floor(Math.random() * 16)),
+        toHex(Math.floor(Math.random() * 16)),
+        toHex(Math.floor(Math.random() * 16)),
+        toHex(Math.floor(Math.random() * 16)),
+    ];
+    const hex = hexArray.reduce((p, v) => `${p}${v}`, '');
+    return `#${hex}`;
+}
+
+function toHex(v) {
+    if (v >= 0 && v <= 9) {
+        return v;
+    }
+
+    return ['a', 'b', 'c', 'd', 'e', 'f'][v - 10];
+}
